3a0ef2784300ff853d08205aedac072911f39a32
Oleksandr Smulsky <dev.postfactum@gmail.com>
2018-10-17 15:17:25 +0200

    initial dev commit, connect CFP and SST

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-
+    
     @include('partials.page-header')
     @include('partials.content-page')
-    
+
     @php echo $builder->init(); @endphp
   
     @endwhile
