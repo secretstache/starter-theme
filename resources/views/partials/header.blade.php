598f3a134f6d3b0639b3f0064a95a73a57b76617
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-01-16 00:36:35 +0200

    integrate header.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,68 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
-</header>
+@php 
+
+$alignment = is_page_template('landing-page.php') ? 'center' : 'justify';
+$link = is_page_template('landing-page.php') ? false : true;
+
+@endphp
+
+@if ( !is_page_template('landing-page.php') && has_nav_menu('primary_navigation') )
+	
+	<div class="off-canvas right" id="offCanvas" data-toggler=".is-active">
+		
+		@php wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'container' => FALSE, 'items_wrap' => '<ul class="vertical menu">%3$s</ul>', 'walker' => new Foundation_Nav_Walker ) ); @endphp
+		
+		<button class="button off-canvas-close" data-toggle="offCanvas">
+			<img src="{{ get_stylesheet_directory_uri() . '/dist/images/x.svg' }}" alt="Close" class="editable">
+		</button>
+	
+	</div>
+
+@endif
+
+<header class="site-header">
+
+	<div class="grid-container">
+
+		<div class="grid-x grid-margin-x align-middle align-{{ $alignment }}">
+			
+			<div class="brand cell shrink">
+			
+				@if ($link)
+					<a href="{{home_url()}}">
+						<span class="site-title">{{get_bloginfo('name')}}</span>
+					</a>
+				@else
+					@if ($logo = get_field('brand_logo', 'options'))
+		
+						@if ($icon = get_field('brand_icon', 'options'))
+							<img src="{{ $logo['url'] }}" alt="{{ $logo['alt'] }}" class="logo show-for-medium">
+							<img src="{{ $icon['url'] }}" alt="{{ $icon['alt'] }}" class="icon hide-for-medium">
+						@else
+							<img src="{{ $logo['url'] }}" alt="{{ $logo['alt'] }}" class="logo">
+						@endif
+					
+					@endif
+						
+				@endif
+		
+			</div>
+	
+			@if ( !is_page_template('landing-page.php') && has_nav_menu('primary_navigation') )
+			
+				<nav class="primary-navigation cell shrink">
+
+					@php wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'container' => FALSE, 'items_wrap' => '<ul class="horizontal menu show-for-medium dropdown" data-dropdown-menu>%3$s</ul>', 'walker' => new Foundation_Nav_Walker ) ); @endphp
+				
+					<button class="hamburger hide-for-medium" type="button" data-toggle="offCanvas" aria-expanded="false" aria-controls="offCanvas">
+						<span class="hamburger-line hamburger-line1"></span>
+						<span class="hamburger-line hamburger-line2"></span>
+						<span class="hamburger-line hamburger-line3"></span>
+					</button>
+				
+				</nav>
+			
+			@endif
+		
+		</div>    
+	</div>
+	</header>
