7b7920312c148807c6635ef2f8405b1221e8f6dd
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-01-17 15:38:54 +0200

    update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,42 @@
 <!doctype html>
+
 <html {{ get_language_attributes() }}>
-  @include('partials.head')
-  <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    <div class="wrap container" role="document">
-      <div class="content">
-        <main class="main">
-          @yield('content')
-        </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
-      </div>
-    </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
-  </body>
+
+	@include('partials.head')
+
+	<body {{ body_class() }} >
+		
+		<div class="off-canvas-wrapper">
+			
+			@include('partials.offcanvas')
+
+			<div class="off-canvas-content" data-off-canvas-content>
+
+				@php do_action('get_header') @endphp
+				@include('partials.header')
+								
+				<div class="wrap container" role="document">
+					<main class="content">
+			
+						@yield('content')
+					
+						@if (App\display_sidebar())
+							<aside class="sidebar">
+								@include('partials.sidebar')
+							</aside>
+						@endif
+
+					</main>
+				</div>
+
+				@php do_action('get_footer') @endphp
+				@include('partials.footer')
+				@php wp_footer() @endphp
+			
+			</div>
+
+		</div>
+
+	</body>
+
 </html>
