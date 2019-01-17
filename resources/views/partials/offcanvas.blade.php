683ef39ebcbdcc469269268bf08753d01095996d
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-01-17 15:40:40 +0200

    add offcanvas.blade.php

---
+++ resources/views/partials/offcanvas.blade.php
@@ -0,0 +1,9 @@
+<div class="off-canvas offcanvas-scale align-center-middle" id="offCanvas" data-off-canvas data-transition="overlap">
+    
+    @php wp_nav_menu( $builder->getMenuArgs('offCanvas') ); @endphp
+    
+	<button class="close-button off-canvas-close" data-toggle="offCanvas">
+		<img src="{{ App\asset_path('images/x.svg') }}" alt="">
+    </button>
+    
+</div>
