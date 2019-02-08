72db7dbbc7e04bdd01519588e7bf335d4ac8a788
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-02-08 18:01:10 +0200

    add switches folder

---
+++ resources/views/switches/modules.blade.php
@@ -0,0 +1,48 @@
+    
+@foreach( $column->modules as $key => $module )
+
+    @php
+        
+        $classes = $builder->getCustomClasses( "module", $key, $module );
+        $id = $builder->getCustomID( $module );
+
+    @endphp
+
+    @switch( $module->acf_fc_layout )
+
+        @case( 'buttons' )
+            
+            @foreach ($module->buttons as $button)
+                
+                @php 
+                
+                    $target = ( $button->option_target == 'new_tab' ) ? '_blank' : '_self'; 
+                    $classes = $builder->getCustomClasses( "module", $key, $button );
+                    $id = $builder->getCustomID( $button );    
+                
+                @endphp
+
+                @include( 'modules.button', [ 'classes' => $classes, 'id' => $id, 'url' => $button->button_url, 'label' => $button->button_label, 'target' => $target ] )
+                
+            @endforeach
+
+            @break
+
+        @case( 'html_editor' )
+
+            @include( 'modules.html-editor', [ 'classes' => $classes, 'id' => $id, 'html_editor' => $module->html_editor_inner_text ] )
+            @break
+
+        @case( 'image' )
+
+            @include( 'modules.image', [ 'classes' => $classes, 'id' => $id, 'src' => $module->image_inner->url, 'alt' => $module->image_inner->alt ] )
+            @break
+
+        @case( 'header' )
+
+            @include( 'modules.header', [ 'classes' => $classes, 'id' => $id, 'headline' => $module->headline, 'subheadline' => $module->subheadline ] )
+            @break
+
+    @endswitch {{-- $module->acf_fc_layout --}}
+
+@endforeach {{-- $modules as $module --}}
---
+++ resources/views/switches/templates.blade.php
@@ -0,0 +1,25 @@
+@php $column_index = 0; @endphp
+	
+@foreach( $templates as $template )
+
+    @switch( $template->acf_fc_layout )
+        
+        @case('columns')
+            
+            @php 
+            
+                $classes = $builder->getCustomClasses( "template", '', $template );
+                $id = $builder->getCustomID( $template );
+                $style = ( $template->option_background == 'Image' && !is_null( $template->option_background_image ) ) ? ' style="background-image: url(' . $template->option_background_image->url . ')" ' : '';
+                $columns_width = $builder->getColumnsWidth( $column_index );
+                
+                $column_index++;
+                
+            @endphp
+
+            @include( 'templates.columns', [ 'classes' => $classes, 'id' => $id, 'style' => $style, 'columns_width' => $columns_width ] )
+            @break
+
+    @endswitch {{-- $template->acf_fc_layout --}}
+
+@endforeach {{-- $templates as $template  --}}
