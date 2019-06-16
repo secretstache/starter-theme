bd14b5b2f29dcc927e00c6827bd07072e8da1de9
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-06-16 17:06:01 +0300

    fixes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
 <footer class="site-footer">
 
 	@if( !$is_landing_page )
-	
+
 		<div class="grid-container">
 
 			<div class="grid-x grid-margin-x">
@@ -11,17 +11,17 @@
 				<div class="cell small-11 medium-4">
 
 					<p><img src="{{ App\asset_path('images/logo.png') }}" alt="" class="footer-logo"></p>
-					
+
 					@if ($footer_summary = get_field('footer_summary', 'options'))
 						{!! $footer_summary !!}
 					@endif
-				
-				</div>	
-		
+
+				</div>
+
 			</div>
-		
+
 		</div>
-	
+
 	@endif
 
 	<div class="bottom">
@@ -29,29 +29,29 @@
 			<div class="grid-x grid-margin-x align-justify align-center">
 
 				@if ( $copyright = get_field('copyright', 'options') )
-					
+
 					<div class="cell shrink">
 						{!! $copyright !!}
 					</div>
-				
+
 				@endif
-				
+
 				@if ( !$is_landing_page )
-					
+
 					<div class="cell shrink">
-						
+
 						<div class="socials">
-							
+
 							<ul>
-								
+
 								@if ( $facebook = get_field('facebook', 'options') )
 									<li><a target="_blank" class="facebook" href="{{ $facebook }}"></a></li>
 								@endif
-								
+
 								@if ( $twitter = get_field('twitter', 'options') )
 									<li><a target="_blank" class="twitter" href="{{ $twitter }}"></a></li>
 								@endif
-								
+
 								@if ( $instagram = get_field('instagram', 'options') )
 									<li><a target="_blank" class="instagram" href="{{ $instagram }}"><i class="fab fa-instagram"></i></a></li>
 								@endif
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-@php 
+@php
 
 $is_landing_page = is_page_template('views/template-landing-page.blade.php') ? true : false;
 $alignment = is_page_template('views/template-landing-page.blade.php') ? 'center' : 'justify';
@@ -11,48 +11,48 @@ $link = is_page_template('views/template-landing-page.blade.php') ? false : true
 	<div class="grid-container">
 
 		<div class="grid-x grid-margin-x align-middle align-{{ $alignment }}">
-			
+
 			<div class="brand cell shrink">
-			
+
 				@if ($link)
 					<a href="{{home_url()}}">
 				@endif
 
 					@if ($logo = get_field('brand_logo', 'options'))
-		
+
 						@if ($icon = get_field('brand_icon', 'options'))
 							<img src="{{ $logo['url'] }}" alt="{{ $logo['alt'] }}" class="logo show-for-medium">
 							<img src="{{ $icon['url'] }}" alt="{{ $icon['alt'] }}" class="icon hide-for-medium">
 						@else
 							<img src="{{ $logo['url'] }}" alt="{{ $logo['alt'] }}" class="logo">
 						@endif
-					
+
 					@else
 						<span class="site-title">{{ get_bloginfo('name') }}</span>
 					@endif
-				
-				@if($link)	
-					</a>						
+
+				@if($link)
+					</a>
 				@endif
-		
+
 			</div>
-	
+
 			@if ( !$is_landing_page && has_nav_menu('primary_navigation') )
-			
+
 				<nav class="primary-navigation cell shrink">
 
 					@php wp_nav_menu( $builder->getMenuArgs('primary_navigation') ); @endphp
-				
+
 					<button class="hamburger hide-for-medium" type="button" data-toggle="offCanvas" aria-expanded="false" aria-controls="offCanvas">
 						<span class="hamburger-line hamburger-line1"></span>
 						<span class="hamburger-line hamburger-line2"></span>
 						<span class="hamburger-line hamburger-line3"></span>
 					</button>
-				
+
 				</nav>
-			
+
 			@endif
-		
-		</div>    
+
+		</div>
 	</div>
 </header>
