65fcbdaf53312ce6fdce18de17138b93232ce78b
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-07-14 15:49:07 +0300

    fix offcanvas

--- resources/views/partials/offcanvas.blade.php
+++ resources/views/partials/offcanvas.blade.php
@@ -1,6 +1,6 @@
 <div class="off-canvas offcanvas-scale align-center-middle" id="offCanvas" data-off-canvas data-transition="overlap">
 
-    @php wp_nav_menu( $builder->getMenuArgs('offCanvas') ); @endphp
+    @php wp_nav_menu( $builder->getMenuArgs('offcanvas') ); @endphp
 
 	<button class="close-button off-canvas-close" data-toggle="offCanvas">
 		<img src="{{ App\asset_path('images/x.svg') }}" alt="">
