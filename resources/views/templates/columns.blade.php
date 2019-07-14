941d8b42674a7b769f1c37fcfff29a5b158404f4
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-07-14 15:53:07 +0300

    remove main from columns template

--- resources/views/templates/columns.blade.php
+++ resources/views/templates/columns.blade.php
@@ -3,21 +3,21 @@
     <section {!! $id !!} {!! $classes !!} {!! $style !!} >
 
         @if( $template->option_background == 'Video' && !is_null( $template->option_background_video ) )
-    
+
             @include( 'partials.video-background', [ 'video' => $template->option_background_video ] )
 
         @endif
-        
+
         @if( $template->option_include_template_header )
-        
+
                 @include( 'partials.template-header', [ 'headline' => $template->option_template_headline, 'subheadline' => $template->option_template_subheadline ] )
-        
+
         @endif {{-- $option_include_template_header --}}
 
         @if( !empty( $template->template_columns ) )
 
             <div class="grid-container">
-                <div class="main grid-x grid-margin-x {{ "align-" . $template->option_x_alignment . " align-" . $template->option_y_alignment . " has-" . count( $template->template_columns ) . "-cols" }} ">
+                <div class="grid-x grid-margin-x {{ "align-" . $template->option_x_alignment . " align-" . $template->option_y_alignment . " has-" . count( $template->template_columns ) . "-cols" }} ">
 
                     @foreach( $template->template_columns as $key => $column )
 
@@ -26,7 +26,7 @@
                         @php $custom_classes = ( $column->option_html_classes ) ? " " . $column->option_html_classes : '' @endphp
 
                         <div {!! $id !!} class="cell small-11 medium-{{ $width . ' i-' . $key }} {{ $column->option_mobile_sort_order }}{{ $custom_classes }}" >
-                            
+
                             <div class="inner">
                                 <div class="content">
 
@@ -39,7 +39,7 @@
                                 </div>
                             </div> {{-- inner --}}
                         </div> {{-- cell --}}
-                        
+
                     @endforeach {{-- $columns as $key => $column --}}
 
                 </div> {{-- main --}}
