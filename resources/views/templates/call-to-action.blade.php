4b7c64922e8984f0242599a7341ed97f5ab1df1e
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-07-31 15:05:14 +0300

    fix cta buttons

--- resources/views/templates/call-to-action.blade.php
+++ resources/views/templates/call-to-action.blade.php
@@ -27,27 +27,54 @@
                                 <div class="component default">
                                     {!! $subheadline !!}
                                 </div>
-                                
+
                             @endif
 
                             @if ( $template->buttons )
-                                
-                                @foreach ($template->buttons as $button)
-                                    @include('modules.button', ["button" => $button] )
-                                @endforeach
+
+								@foreach ($template->buttons as $button)
+
+									@php
+
+										$module = (object)array(
+											"acf_fc_layout" => "buttons"
+										);
+
+										$wrapper_id = $builder->getCustomID( $module );
+										$wrapper_classes = $builder->getCustomClasses( "module", $button->option_button_alignment, $key, $module );
+
+										$inner_id = $builder->getCustomID( $button );
+										$inner_classes = ( $button->option_html_classes ) ? " " . $button->option_html_classes : '';
+										$size_class = ($button->option_button_size) ? " " . $button->option_button_size : '';
+
+									@endphp
+
+									@include('modules.button', ["button" => $button, 'inner_id' => $inner_id, 'inner_classes' => $inner_classes, 'size_class' => $size_class ] )
+
+								@endforeach
+
+
+
+								@foreach ($module->buttons as $button)
+
+									@include( 'modules.button', [ 'button' => $button, 'inner_id' => $inner_id, 'inner_classes' => $inner_classes, 'size_class' => $size_class ] )
+
+								@endforeach
+
+
 
                             @endif
-                        
+
                         </div>
-            
+
                     </div>
-            
+
                 </div>
-            
+
             </div>
 
         </div>
-    
+
     </section>
 
 @endif
