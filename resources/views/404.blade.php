92025e794c531edad6ef041f258154b081530241
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-08-03 14:33:45 +0300

    add default 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -2,16 +2,31 @@
 
 @section('content')
 
-	@include('partials.page-header')
+	<header class="page-header" style="margin-top:120px">
 
-	@if (!have_posts())
+		<div class="grid-container">
+
+			<div class="grid-x grid-margin-x align-center medium-align-left">
+
+				<div class="cell small-12 medium-12">
+					<h1 class="headline">Not Found</h1>
+				</div>
+
+			</div>
+
+		</div>
+
+	</header>
+
+	<div class="grid-container" style="margin-bottom:120px">
+
+		@if (!have_posts())
 
 		<div class="alert alert-warning">
 			{{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
 		</div>
 
-		{!! get_search_form(false) !!}
+	</div>
 
-	@endif
-
+  @endif
 @endsection
