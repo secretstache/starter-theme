d48cd14a2ee0165f22ab73b3cefc81f6fef47677
Oleksandr Smulsky <dev.postfactum@gmail.com>
2019-09-30 21:19:39 +0300

    form adjustments

--- resources/views/modules/map.blade.php
+++ resources/views/modules/map.blade.php
@@ -2,15 +2,14 @@
 
     @if ( $physical_address = get_field('physical_address', 'options') )
 
-
         @if ($builder)
-        
+
             <div id="map" style="position: relative; overflow: hidden;">
-                <iframe class="gmap" src="https://www.google.com/maps?q=<?php echo $builder->getPreparedAddress( $physical_address ) ?>&output=embed"></iframe>
+                <iframe class="gmap" src="https://www.google.com/maps?q=<?php echo $builder->getMapAddress( $physical_address ) ?>&output=embed"></iframe>
             </div>
-        
-            <p><a target="_blank" class="get-directions" href="https://maps.google.com/?q=<?php echo $builder->getPreparedAddress( $physical_address ) ?>">Get Directions</a></p>		
-        
+
+            <p><a target="_blank" class="get-directions" href="https://maps.google.com/?q=<?php echo $builder->getMapAddress( $physical_address ) ?>">Get Directions</a></p>
+
         @endif
 
     @endif
